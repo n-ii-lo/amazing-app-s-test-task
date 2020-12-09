02da8a0e0ca9c13b8d5bba7e553a74eeb33bf8e8
n-ii-lo <alyaska56@gmail.com>
2020-12-09 20:10:09 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $("#main-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    speed: 2000,
+		swipe: false,
+  });
+});
+//=======================================================================================
+$("img.svg").each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(
+    imgURL,
+    function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgClass !== "undefined") {
+        $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+        $svg.attr(
+          "viewBox",
+          "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
+        );
+      }
+      $img.replaceWith($svg);
+    },
+    "xml"
+  );
+});
